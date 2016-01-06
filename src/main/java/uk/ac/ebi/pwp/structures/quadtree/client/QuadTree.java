b4ea9255e5a0c77634421d78dda5d39f16eef194
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-01-06 13:26:03 +0000

    minor changes

--- src/main/java/uk/ac/ebi/pwp/structures/quadtree/client/QuadTree.java
+++ src/main/java/uk/ac/ebi/pwp/structures/quadtree/client/QuadTree.java
@@ -10,35 +10,35 @@ public class QuadTree<T extends QuadTreeBox> {
     private QuadNode<T> root = null;
 
     public QuadTree(double minX, double minY, double maxX, double maxY){
-        this.root = new QuadNode<>(minX, minY, maxX, maxY, 5);
+        root = new QuadNode<>(minX, minY, maxX, maxY, 5, 25);
     }
 
-    public QuadTree(double minX, double minY, double maxX, double maxY, int numberOfItems){
-        this.root = new QuadNode<>(minX, minY, maxX, maxY, numberOfItems);
+    public QuadTree(double minX, double minY, double maxX, double maxY, int numberOfItems, int minArea){
+        root = new QuadNode<>(minX, minY, maxX, maxY, numberOfItems, minArea);
     }
 
     public QuadTree(double width, double height) {
         this(0, 0, width, height);
     }
 
-    public QuadTree(double width, double height, int numberOfItems) {
-        this(0, 0, width, height, numberOfItems);
+    public QuadTree(double width, double height, int numberOfItems, int minArea) {
+        this(0, 0, width, height, numberOfItems, minArea);
     }
 
     public boolean add(T item){
-        return this.root.add(new QuadItem<>(item));
+        return root.add(new QuadItem<>(item));
     }
 
     public boolean remove(T item){
-        return this.root.remove(item);
+        return root.remove(item);
     }
 
     public Set<T> getItems(double x, double y){
-        return this.root.getItems(x,y);
+        return root.getItems(x,y);
     }
 
     public Set<T> getItems(QuadTreeBox area){
-        return this.root.getItems(area);
+        return root.getItems(area);
     }
 
 }
